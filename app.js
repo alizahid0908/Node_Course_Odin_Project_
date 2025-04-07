adb0cae3bea30354182aa83627569c0babda9b6a
alizahid0908 <alizahid0908@proton.me>
2025-04-07 10:42:14 -0700

    express basics

--- app.js
+++ app.js
@@ -13,12 +13,27 @@ const __dirname = path.dirname(__filename);
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
+app.assetsPath = path.join(__dirname, "public")
+app.use(express.static(app.assetsPath))
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/authors", text: "Authors" },
+  { href: "/books", text: "Books" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
 app.get("/", (req, res) => {
-    res.render("index", { message: "EJS rocks!" });
-  });       
+  res.render("index", { links: links, users: users });
+  }); 
+app.get("/authors", (req, res) => {
+  res.render("authors", { links: links, users: users });
+});  
+    
 // app.use("/", indexRouter);
-app.use("/authors", authorRouter);
-app.use("/books", bookRouter);
+// app.use("/authors", authorRouter);
+// app.use("/books", bookRouter);
 
 const PORT = 3000; 
 
